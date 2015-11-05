bde3b6f50e3609155708aded69516abebf931e72
Menglong Woo <MenglongWoo@aliyun.com>
2015-11-05 13:48:39 +0800

    Stele 为SSD1963优化ucgui驱动

--- uCGUI/GUI/LCDDriver/LCDDummy.c
+++ uCGUI/GUI/LCDDriver/LCDDummy.c
@@ -32,6 +32,29 @@ Version-Date---Author-Explanation
 1.00.00 020417 JE     a) Changed to have only to adapt _GetPixelIndex
                          and _SetPixelIndex
 0.90.00 020214 JE     a) First release
+
+
+
+
+
+
+- 2015-11-05,Menglong Woo, MenglongWoo@aliyun.com
+  - SSD1963 图像处理器特质驱动优化部分包括
+      - LCD_Area
+      - _DrawBitLine1BPP
+        - 普通模式
+        - 透明模式
+        - 不包括异或模式LCD_DRAWMODE_XOR
+      - 
+      - _DrawBitLine2BPP
+        - 普通模式
+        - 透明模式
+      - LCD_L0_SetPixelIndex
+      - LCD_L0_GetPixelIndex
+      - LCD_L0_XorPixel
+      - LCD_L0_DrawHLine
+      - LCD_L0_DrawVLine
+      - LCD_L0_FillRect
 ---------------------------END-OF-HEADER------------------------------
 */
 
@@ -41,7 +64,7 @@ Version-Date---Author-Explanation
 
 // Menglong Woo
 #include "./USER/GLCD/glcd.h"
-
+#include "LCD/gl_ui.h"
 #if (LCD_CONTROLLER == -1) \
     && (!defined(WIN32) | defined(LCD_SIMCONTROLLER))
 
@@ -105,7 +128,11 @@ Version-Date---Author-Explanation
 *
 **********************************************************************
 */
-
+static __inline void LCD_Area(uint16_t x0,uint16_t y0,uint16_t x1,uint16_t y1)
+{
+  LCD_SetArea(x0,y0,x1,y1);
+  LCD_WriteCommand(CMD_WR_MEMSTART);
+}
 /*********************************************************************
 *
 *       Draw Bitmap 1 BPP
@@ -114,41 +141,68 @@ static void  _DrawBitLine1BPP(int x, int y, U8 const GUI_UNI_PTR *p, int Diff, i
   LCD_PIXELINDEX Index0 = *(pTrans+0);
   LCD_PIXELINDEX Index1 = *(pTrans+1);
   x += Diff;
+  
+  // printf("start %d %d %d %d\n", x, y, Diff,xsize);
   switch (GUI_Context.DrawMode & (LCD_DRAWMODE_TRANS | LCD_DRAWMODE_XOR)) {
     case 0:
+      LCD_Area(x,y,xsize,y);
       do {
-        LCD_L0_SetPixelIndex(x++, y, (*p & (0x80 >> Diff)) ? Index1 : Index0);
-			  if (++Diff == 8) {
+        LCD_WriteData((*p & (0x80 >> Diff)) ? Index1 : Index0);
+        if (++Diff == 8) {
           Diff = 0;
-				  p++;
-			  }
-		  } while (--xsize);
+          p++;
+        }
+      } while (--xsize);
       break;
     case LCD_DRAWMODE_TRANS:
-      do {
-  		  if (*p & (0x80 >> Diff))
-          LCD_L0_SetPixelIndex(x, y, Index1);
-        x++;
-			  if (++Diff == 8) {
-          Diff = 0;
-				  p++;
-			  }
-		  } while (--xsize);
+      {
+        int k = 1;
+        do {
+          
+          if (*p & (0x80 >> Diff)) {
+            if (k) {
+              LCD_Area(x,y,x + xsize,y);
+              k = 0;
+            }
+            LCD_WriteData(Index1);
+          }
+          else {
+            k++;
+          }
+          x++;
+          if (++Diff == 8) {
+            Diff = 0;
+            p++;
+          }
+        } while (--xsize);
+      }
+
+      // do {
+      //   if (*p & (0x80 >> Diff)) {
+      //     LCD_L0_SetPixelIndex(x, y, Index1);
+      //   }
+      //   x++;
+      //   if (++Diff == 8) {
+      //     Diff = 0;
+      //     p++;
+      //   }
+      // } while (--xsize);
       break;
     case LCD_DRAWMODE_XOR:;
       do {
-  		  if (*p & (0x80 >> Diff)) {
+        if (*p & (0x80 >> Diff)) {
           int Pixel = LCD_L0_GetPixelIndex(x, y);
           LCD_L0_SetPixelIndex(x, y, LCD_NUM_COLORS - 1 - Pixel);
         }
         x++;
-			  if (++Diff == 8) {
+        if (++Diff == 8) {
           Diff = 0;
-				  p++;
-			  }
-		  } while (--xsize);
+          p++;
+        }
+      } while (--xsize);
       break;
-	}
+  }
+  // printf("end %d %d %d %d\n", x, y, Diff,xsize);
 }
 
 /*********************************************************************
@@ -163,60 +217,83 @@ static void  _DrawBitLine2BPP(int x, int y, U8 const GUI_UNI_PTR * p, int Diff, 
   switch (GUI_Context.DrawMode & (LCD_DRAWMODE_TRANS | LCD_DRAWMODE_XOR)) {
     case 0:
       if (pTrans) {
+        LCD_Area(x,y,xsize*2,y);
         do {
           int Shift = (3 - CurrentPixel) << 1;
           int Index = (Pixels & (0xC0 >> (6 - Shift))) >> Shift;
           LCD_PIXELINDEX PixelIndex = *(pTrans + Index);
-          LCD_L0_SetPixelIndex(x++, y, PixelIndex);
+          // LCD_L0_SetPixelIndex(x++, y, PixelIndex);
+          LCD_WriteData(PixelIndex);
           if (++CurrentPixel == 4) {
             CurrentPixel = 0;
             Pixels = *(++p);
           }
-		    } while (--xsize);
+        } while (--xsize);
       } else {
+        LCD_Area(x,y,xsize*2,y);
         do {
           int Shift = (3 - CurrentPixel) << 1;
           int Index = (Pixels & (0xC0 >> (6 - Shift))) >> Shift;
-          LCD_L0_SetPixelIndex(x++, y, Index);
+          // LCD_L0_SetPixelIndex(x++, y, Index);
+          LCD_WriteData(Index);
           if (++CurrentPixel == 4) {
             CurrentPixel = 0;
             Pixels = *(++p);
           }
-		    } while (--xsize);
+        } while (--xsize);
       }
       break;
     case LCD_DRAWMODE_TRANS:
       if (pTrans) {
+        int k = 1;
         do {
           int Shift = (3 - CurrentPixel) << 1;
           int Index = (Pixels & (0xC0 >> (6 - Shift))) >> Shift;
           if (Index) {
             LCD_PIXELINDEX PixelIndex = *(pTrans + Index);
-            LCD_L0_SetPixelIndex(x, y, PixelIndex);
+            // LCD_L0_SetPixelIndex(x, y, PixelIndex);
+            if (k) {
+              LCD_Area(x,y,(x + xsize)*2,y);
+              k = 0;
+            }
+            LCD_WriteData(PixelIndex);
+          }
+           else {
+              k++;
           }
           x++;
           if (++CurrentPixel == 4) {
             CurrentPixel = 0;
             Pixels = *(++p);
           }
-		    } while (--xsize);
+        } while (--xsize);
       } else {
+        int k = 1;
         do {
           int Shift = (3 - CurrentPixel) << 1;
           int Index = (Pixels & (0xC0 >> (6 - Shift))) >> Shift;
           if (Index) {
-            LCD_L0_SetPixelIndex(x, y, Index);
+            // LCD_L0_SetPixelIndex(x, y, Index);
+            if (k) {
+              LCD_Area(x,y,(x + xsize)*2,y);
+              k = 0;
+            }
+            LCD_WriteData(Index);
+          }
+          else {
+              k++;
           }
           x++;
           if (++CurrentPixel == 4) {
             CurrentPixel = 0;
             Pixels = *(++p);
           }
-		    } while (--xsize);
+        } while (--xsize);
       }
-      break;
+        break;
   }
 }
+
 #endif
 
 /*********************************************************************
@@ -382,6 +459,7 @@ static void  DrawBitLine16BPP(int x, int y, U16 const GUI_UNI_PTR * p, int xsize
 *   calling this routine make sure that the coordinates are in range, so
 *   that no check on the parameters needs to be performed.
 */
+int g_color = 0;
 void LCD_L0_SetPixelIndex(int x, int y, int PixelIndex) {
   /* Convert logical into physical coordinates (Dep. on LCDConf.h) */
   #if LCD_SWAP_XY | LCD_MIRROR_X| LCD_MIRROR_Y
@@ -394,9 +472,9 @@ void LCD_L0_SetPixelIndex(int x, int y, int PixelIndex) {
   /* Write into hardware ... Adapt to your system */
   {
     /* ... */
-//	LCD_SetPoint(x,y,PixelIndex);
-	gl_setarea(x,y,x+1,y+1);
-	gl_setpoint(x,y,PixelIndex);
+  // gl_setpoint(x,y,PixelIndex);
+  	LCD_Area(x,y,x+1,y+1);  
+    LCD_WriteData(PixelIndex);
   }
 }
 
@@ -421,7 +499,9 @@ unsigned int LCD_L0_GetPixelIndex(int x, int y) {
   #endif
   /* Read from hardware ... Adapt to your system */
   {
-    PixelIndex = 0;/* ... */
+    // PixelIndex = 0;/* ... */
+    PixelIndex = LCD_GetPoint(x,y);
+    // PixelIndex = 0;
   }
   return PixelIndex;
 }
@@ -445,8 +525,15 @@ void LCD_L0_DrawHLine  (int x0, int y,  int x1) {
       LCD_L0_XorPixel(x0, y);
     }
   } else {
+    // for (; x0 <= x1; x0++) {
+    //   LCD_L0_SetPixelIndex(x0, y, LCD_COLORINDEX);
+    // }
+
+    LCD_Area(x0, y, x1, y);
+    
+    
     for (; x0 <= x1; x0++) {
-      LCD_L0_SetPixelIndex(x0, y, LCD_COLORINDEX);
+        LCD_WriteData(LCD_COLORINDEX);
     }
   }
 }
@@ -461,8 +548,14 @@ void LCD_L0_DrawVLine  (int x, int y0,  int y1) {
       LCD_L0_XorPixel(x, y0);
     }
   } else {
+    // for (; y0 <= y1; y0++) {
+    //   LCD_L0_SetPixelIndex(x, y0, LCD_COLORINDEX);
+    // }
+    LCD_Area(x, y0, x, y1);
+    
+    
     for (; y0 <= y1; y0++) {
-      LCD_L0_SetPixelIndex(x, y0, LCD_COLORINDEX);
+        LCD_WriteData(LCD_COLORINDEX);
     }
   }
 }
@@ -472,8 +565,20 @@ void LCD_L0_DrawVLine  (int x, int y0,  int y1) {
 *       LCD_L0_FillRect
 */
 void LCD_L0_FillRect(int x0, int y0, int x1, int y1) {
-  for (; y0 <= y1; y0++) {
-    LCD_L0_DrawHLine(x0, y0, x1);
+  // ver 1
+  // for (; y0 <= y1; y0++) {
+  //   LCD_L0_DrawHLine(x0, y0, x1);
+  // }
+  // return ;
+
+  // ver 5
+  int t;
+  LCD_Area(x0, y0, x1, y1);
+  
+  
+  t = ((x1 - x0) +1 ) * ((y1 -y0) +1);
+  for ( ; t--; ) {
+      LCD_WriteData(LCD_COLORINDEX);
   }
 }
 
@@ -489,6 +594,14 @@ void LCD_L0_DrawBitmap(int x0, int y0,
                        const LCD_PIXELINDEX* pTrans)
 {
   int i;
+//   printf("x0 %d\ny0 %d\nxsize %d\nysize %d\nBitsPerPixel %d\nBytesPerLine %d\nDiff %d \n",
+// x0,
+// y0,
+// xsize,
+// ysize,
+// BitsPerPixel,
+// BytesPerLine,
+// Diff);
   /* Use _DrawBitLineXBPP */
   for (i=0; i<ysize; i++) {
     switch (BitsPerPixel) {
